260071728c3c84007248bda48a42fc806c60eff0
nilanoyon371-afk <nilanoyon371@gmail.com>
2026-03-03 00:51:26 +0600

    Update apphub_version.py

--- app/apphub_version.py
+++ app/apphub_version.py
@@ -2,7 +2,7 @@
 # Update this file when you release a new version of AppHub
 
 VERSION = "3.0.0"
-BUILD_NUMBER = 3
+BUILD_NUMBER = 2
 DOWNLOAD_URL = "http://live-4k.xyz/apphub/app/app-arm64-v8a-release.apk"
 CHANGELOG_TITLE = "🎉 What's New in v3.0.0"
 CHANGELOG = """
@@ -34,5 +34,5 @@ Fixed offline launch crashes.
 
 💡 Pro Tip: Using a VPN is highly recommended to bypass provider blocks and ensure 100% success with downloads and thumbnails.
 """
-IS_MANDATORY = False
+IS_MANDATORY = True
 SIZE_BYTES = 28000000  # ~38 MB
